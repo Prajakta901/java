8e18485f263119014308de85683e929f89842bbf
Prajakta901 <prajaktasambare.24@stvincentngp.edu.in>
2025-11-30 16:21:01 +0530

    Diamond Pattern

--- 1st.java
+++ 1st.java
@@ -1,22 +1,43 @@
 import java.util.*;
 
-class st{
-    
-    public static  void main(String args[]){
-    
-         for (int i = 1; i <=5; i++){
+class css{
+    public static void main(String args[]){
+        int i, j;
+        int n =4;
+         for(i=1;i<=n;i++){
+            //space
+            for(j = 1;j <=n-i ;j++){
+                System.out.print(" ");
+            }
+               
+                 for(j = i ;j>1; j--){
+                   System.out.print("*" );
 
-            for (int j = 1; j <=i; j++){
-                int sum = i+j ;
-                if(sum %2 == 0){
-                System.out.print("1 ");
-                }
-                else{
-                    System.out.print("0 ");
-            }        
+                 }
+                 
+                 for(j = 1 ;j<=i; j++){
+                   System.out.print("*" );
+
+                 }
+               
+                System.out.println( );
+
+            }
+             for(i=n;i>=1;i--){
+            //space
+            for(j = 1;j <=n-i ;j++){
+                System.out.print(" ");
+            }
+              
+                 for(j = 1 ;j<=2*i-1; j++){
+                   System.out.print("*" );
+
+                 }                 
+                
+               
+                System.out.println( );
+               }
+             
          }
-          System.out.println();   
-    }
-}
-}
 
+}
