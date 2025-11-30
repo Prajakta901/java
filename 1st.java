d30314125aaabb639497d1f87a5a587235d79d4e
Prajakta901 <prajaktasambare.24@stvincentngp.edu.in>
2025-11-30 16:47:50 +0530

    Input

--- 1st.java
+++ 1st.java
@@ -1,43 +1,10 @@
 import java.util.*;
-
-class css{
-    public static void main(String args[]){
-        int i, j;
-        int n =4;
-         for(i=1;i<=n;i++){
-            //space
-            for(j = 1;j <=n-i ;j++){
-                System.out.print(" ");
-            }
-               
-                 for(j = i ;j>1; j--){
-                   System.out.print("*" );
-
-                 }
-                 
-                 for(j = 1 ;j<=i; j++){
-                   System.out.print("*" );
-
-                 }
-               
-                System.out.println( );
-
-            }
-             for(i=n;i>=1;i--){
-            //space
-            for(j = 1;j <=n-i ;j++){
-                System.out.print(" ");
-            }
-              
-                 for(j = 1 ;j<=2*i-1; j++){
-                   System.out.print("*" );
-
-                 }                 
-                
-               
-                System.out.println( );
-               }
-             
-         }
-
+class prin{
+public static void main(String args[]) {
+  int i ;
+  Scanner sc = new Scanner(System.in);
+  System.out.print("Enter a number: ");
+  i = sc.nextInt();
+  System.out.println("Your Entered " + i);
+}
 }
